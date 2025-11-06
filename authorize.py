60177b8f3338f27023835952d8f33bc7a7a4f53a
ethanzhang <ezhang30@terpmail.umd.edu>
2025-11-06 18:49:49 -0500

    Trying to fix server error

--- authorize.py
+++ authorize.py
@@ -1,18 +1,21 @@
-from flask_login import current_user
-from flask import render_template, flash, current_app
 from functools import wraps
+from flask import flash, render_template
+from flask_login import current_user
 
 def role_required(roles):
+    """Decorator to require specific roles for access to a route"""
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if current_user and current_user.role in roles:
-                pass
-            else:
+            if not current_user.is_authenticated:
+                flash('You must be logged in to access this page.', 'error')
+                return render_template('access_denied.html')
+
+            if current_user.role not in roles:
                 flash('You do not have permission to access this function.', 'error')
                 return render_template('access_denied.html')
-            if callable(getattr(current_app, "ensure_sync", None)):
-                return current_app.ensure_sync(func)(*args, **kwargs)
+
             return func(*args, **kwargs)
         return wrapper
     return decorator
+
